729589a40d7ba97252606b8d1da4da0a182546b4
Admin <Admin@DESKTOP-OOAIQ5U>
2024-02-04 21:27:09 +0300

    .

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RegPageDiana
 {
@@ -7,6 +10,17 @@ namespace RegPageDiana
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Register(object sender, RoutedEventArgs e)
+        {
+            Window1 w1 = new Window1();
+            w1.Text1.Text = FirstName.Text;
+            w1.Text2.Text = LastName.Text;
+            w1.Email.Text = Email.Text;
+            w1.Password.Text = Password.Text;
+
+            w1.Show();
         }
     }
 }
